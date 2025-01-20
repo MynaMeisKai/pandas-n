dea9a2b5786dbec1d833ccbf9234b288077350e8
MynaMeisKai <saijanarthanan217@gmail.com>
2025-01-20 07:46:12 +0530

    pandas-loc,iloc

--- index.py
+++ index.py
@@ -5,6 +5,13 @@ df = pd.DataFrame([[12,42,64],[32,55,75],[39,0,33]])
 
 coff = pd.read_csv('https://raw.githubusercontent.com/KeithGalli/complete-pandas-tutorial/refs/heads/master/warmup-data/coffee.csv')
 
-#print(coff)
-#print(coff.loc[2:7])
-#print(coff["Day"].to_list())
+#loc & iloc
+
+#print(coff.loc[1:7,["Day","Units Sold"]])
+
+#loc gives o/p for row =1 to 7  columns as the name in DF
+
+#print(coff.iloc[1:5,[0,1]])
+
+#iloc only numeric in rows and columns row denoted
+# as 1:3 and columns as [0,2]
