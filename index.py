9ff0ffda2496e062d5827924b1f493e5ce304961
MynaMeisKai <saijanarthanan217@gmail.com>
2025-01-20 07:24:18 +0530

    pandas-B

---
+++ index.py
@@ -0,0 +1,10 @@
+
+import pandas as pd
+
+df = pd.DataFrame([[12,42,64],[32,55,75],[39,0,33]])
+
+coff = pd.read_csv('https://raw.githubusercontent.com/KeithGalli/complete-pandas-tutorial/refs/heads/master/warmup-data/coffee.csv')
+
+#print(coff)
+#print(coff.loc[2:7])
+#print(coff["Day"].to_list())
