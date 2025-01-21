a64238dd41c44d6c5f6f8ca5d11ea0a3f1847486
MynaMeisKai <saijanarthanan217@gmail.com>
2025-01-21 07:57:46 +0530

    pandas-at,iat

--- index.py
+++ index.py
@@ -5,13 +5,12 @@ df = pd.DataFrame([[12,42,64],[32,55,75],[39,0,33]])
 
 coff = pd.read_csv('https://raw.githubusercontent.com/KeithGalli/complete-pandas-tutorial/refs/heads/master/warmup-data/coffee.csv')
 
-#loc & iloc
+#at & iat
 
-#print(coff.loc[1:7,["Day","Units Sold"]])
+print(coff)
+print(coff.at[1,"Units Sold"])
 
-#loc gives o/p for row =1 to 7  columns as the name in DF
-
-#print(coff.iloc[1:5,[0,1]])
-
-#iloc only numeric in rows and columns row denoted
-# as 1:3 and columns as [0,2]
+#iat rows and columns must be in numerics
+print(coff.iat[0,2])
+#at and iat only gives exact single output 
+#not multiple
