0e4320120f40d5bcf42641cf9e08db4290922bd6
MynaMeisKai <saijanarthanan217@gmail.com>
2025-01-23 06:18:56 +0530

    pandas-add/remove

--- index.py
+++ index.py
@@ -1,13 +1,19 @@
 import pandas as pd
+import numpy as np
 
 df = pd.DataFrame([[12,42,64],[32,55,75],[39,0,33]])
 
 coff = pd.read_csv('https://raw.githubusercontent.com/KeithGalli/complete-pandas-tutorial/refs/heads/master/warmup-data/coffee.csv')
 bios = pd.read_csv('https://raw.githubusercontent.com/KeithGalli/complete-pandas-tutorial/refs/heads/master/data/bios.csv')
-# sort ,ascending 
+# Add / Remove Columns
 
-#sort one column
-#print(coff.sort_values("Units Sold",ascending=False))
+coff["Price"] = np.where(coff["Coffee Type"] == "Latte",3.99,5.99)
 
-#sort two column
-print(coff.sort_values(["Units Sold","Coffee Type"],ascending=[0,1]))
+#print(coff.head(3))
+
+#delete 
+
+#coff.drop(2)
+#print(coff.drop(columns=["Units Sold"]))
+print(coff.drop(columns=["Units Sold"],inplace= True))
+print(coff.head())
